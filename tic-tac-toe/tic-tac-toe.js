79a975be9f5f10048ec7f72888ffd69a79f4f358
Parth-RK <2021bcs119@sggs.ac.in>
2024-07-07 22:45:51 +0530

    Added an undo button

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -2,6 +2,8 @@ const container = document.querySelector('.container');
 const message = document.querySelector('.message');
 let currentPlayer = 'X';
 let playerMoves = { 'X': [], 'O': [] };
+let gameHistory = [];
+let gameOver = false;
 const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -23,6 +25,7 @@ function checkWin(player) {
 function resetGame() {
     currentPlayer = 'X';
     playerMoves = { 'X': [], 'O': [] };
+    gameHistory = [];
     message.textContent = '';
     container.style.pointerEvents = 'auto';
     document.querySelectorAll('.cell').forEach(cell => {
@@ -32,11 +35,55 @@ function resetGame() {
     document.getElementById('currentPlayer').textContent = currentPlayer;
 }
 
+function undoMove() {
+    if (gameHistory.length > 0 && !gameOver) {
+        const lastMove = gameHistory.pop();
+        const cell = lastMove.cell;
+        const player = lastMove.player;
+
+        cell.textContent = '';
+        cell.classList.remove(player);
+
+        playerMoves[player] = playerMoves[player].filter((cell) => cell !== lastMove.cell);
+
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        document.getElementById('currentPlayer').textContent = currentPlayer;
+
+        message.textContent = '';
+        container.style.pointerEvents = 'auto';
+        
+    }
+    else{
+        if(gameOver){
+            const lastMove = gameHistory.pop();
+        const cell = lastMove.cell;
+        const player = lastMove.player;
+
+        
+        cell.textContent = '';
+        cell.classList.remove(player);
+
+        
+        playerMoves[player] = playerMoves[player].filter((cell) => cell !== lastMove.cell);
+
+        message.textContent = '';
+        container.style.pointerEvents = 'auto';
+        gameOver = false;
+        }
+    }
+}
+
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('cell') && !e.target.textContent && !message.textContent) {
         e.target.textContent = currentPlayer;
         e.target.classList.add(currentPlayer);
         playerMoves[currentPlayer].push(e.target);
+
+        gameHistory.push({
+            player: currentPlayer,
+            cell: e.target,
+            index: parseInt(e.target.getAttribute('data-index')),
+            });
 
         if (playerMoves[currentPlayer].length > 3) {
             const firstMove = playerMoves[currentPlayer].shift();
@@ -55,6 +102,7 @@ container.addEventListener('click', (e) => {
         if (checkWin(currentPlayer)) {
             message.textContent = `Player ${currentPlayer} wins!`;
             container.style.pointerEvents = 'none';
+            gameOver = true;
         } else {
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
             document.getElementById('currentPlayer').textContent = currentPlayer;
