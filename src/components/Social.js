6b0f24604174b1e0b306a4b8ae50f668587491e3
Parth-RK <2021bcs119@sggs.ac.in>
2024-12-27 14:48:40 +0530

    social links

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,13 +4,13 @@ import React from 'react';
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="https://github.com/YourUsername" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/Parth-RK" className="home__social-icon" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-github"></i>
       </a>
-      <a href="https://linkedin.com/in/YourUsername" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+      <a href="https://linkedin.com/in/parth-rk" className="home__social-icon" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-linkedin"></i>
       </a>
-      <a href="mailto:your.email@example.com" className="home__social-icon">
+      <a href="mailto:katkeparth@gmail.com" className="home__social-icon">
         <i className="fas fa-envelope"></i>
       </a>
     </div>
